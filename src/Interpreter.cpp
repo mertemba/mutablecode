7a2f3dd27348c92c7218806fb299b63f754e6cd4
jellysheep <mail@sheepstyle.comeze.com>
2013-10-15 18:03:25 +0200

    Added README and LICENSE files. Added license headers to code.

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -1,3 +1,12 @@
+/**
+ * \file
+ * \author jellysheep, ClundXIII, edlervonhuber.
+ * \section LICENSE
+ * Copyright (c) 2013, jellysheep, ClundXIII, edlervonhuber.
+ * All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
 
 #include <Interpreter.hpp>
 #include <Program.hpp>
