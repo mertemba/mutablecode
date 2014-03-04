55cd599bf70482bc9fadd12b91b8add4fc04561c
jellysheep <max.mail@dameweb.de>
2014-03-04 17:41:35 +0100

    Updated copyright notice year.

--- include/Char.hpp
+++ include/Char.hpp
@@ -2,7 +2,7 @@
  * \file
  * \author jellysheep, ClundXIII, edlervonhuber.
  * \section LICENSE
- * Copyright (c) 2013, jellysheep, ClundXIII, edlervonhuber.
+ * Copyright (c) 2014, jellysheep, ClundXIII, edlervonhuber.
  * All rights reserved.
  * This file is licensed under the "BSD 3-Clause License".
  * Full license text is under the file "LICENSE" provided with this code.
